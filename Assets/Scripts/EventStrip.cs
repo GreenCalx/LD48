23011c04bad0ec2442b3de4312db0d84eb6ebce4
greencalx <epileptik.2@gmail.com>
2021-04-25 15:03:55 +0200

    event strip okay + scan branching on enemybehavior + spacedummy dying

--- Assets/Scripts/EventStrip.cs
+++ Assets/Scripts/EventStrip.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(RectTransform))]
 public class EventStrip : MonoBehaviour
 {
     public const int MIN_MENACE = 1;
@@ -14,14 +16,15 @@ public class EventStrip : MonoBehaviour
     public int menace_density;
 
     public GameObject refEventCollection;
-
+    public GameObject refShip;
 
     [HideInInspector]
     public float event_cooldown;
     private float cooldown_before_next_event;
     private bool strip_is_active;
-
     
+    private BoxCollider2D bc2d;
+    private RectTransform rectTransform;
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +36,13 @@ public class EventStrip : MonoBehaviour
         // if unset we try to retrieve collec from scene.
         if (refEventCollection == null)
             refEventCollection = GameObject.Find("EventCollection");
+
+        if (refShip == null)
+            refShip = GameObject.Find("Ship");
+        
+        bc2d = GetComponent<BoxCollider2D>();
+        rectTransform = GetComponent<RectTransform>();
+
     }
 
     // Update is called once per frame
@@ -62,13 +72,28 @@ public class EventStrip : MonoBehaviour
                 return;
             }
             GameObject menace = tc.getRandomMenace(threat_level);
-            Instantiate(menace);
-
-            // TODO, direction and spawning pos..
+            if (menace!=null)
+                spawnMenace(menace);
+            else
+                Debug.LogWarning("Menace from collection is null. nothing will happen.");
 
         } else {
             Debug.LogWarning("EventCollection not found for strip");
         }
+    }
+
+    private void spawnMenace( GameObject iPrefab )
+    {
+
+        // Set position somewhere in the strip
+        Vector3 position = new Vector3( Random.Range(rectTransform.rect.xMin, rectTransform.rect.xMax),
+                                        Random.Range(rectTransform.rect.yMin, rectTransform.rect.yMax),
+                                        0) + rectTransform.transform.position;
+        
+        GameObject new_menace = Instantiate(iPrefab, position, Quaternion.identity);
+
+        // Set direction towards player ? Init Menace behaviour accordingly?
+
     }
 
     private void OnTriggerStay2D( Collider2D iCol )
