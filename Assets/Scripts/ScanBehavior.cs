2e8775f90ddd88346c5df6704638ff07ef09a0a7
Toffanim <marc.toffanin@gmail.com>
2021-04-26 17:03:59 +0200

    making the radar sexier

--- Assets/Scripts/ScanBehavior.cs
+++ Assets/Scripts/ScanBehavior.cs
@@ -23,6 +23,7 @@ public class ScanBehavior : ShipElem
     public float Speed;
     public int DotSize;
     public Texture2D mEnnemieSprite;
+    public Texture2D mEnnemiesOOBSprite;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,17 +42,33 @@ public class ScanBehavior : ShipElem
 
     void PrintDot(int x, int y, int GaussianDistanceRadiusInPixel, Color32 Color)
     {
-        float x_ClipSpace = (float)x / mImage.width;
-        float y_ClipSpace = (float)y / mImage.height;
+        float x_ClipSpace = Mathf.Clamp((float)x / mImage.width, 0.1f, 0.9f);
+        float y_ClipSpace = Mathf.Clamp((float)y / mImage.height, 0.1f, 0.9f);
+
+        bool IsArrow = (x_ClipSpace != (float)x / mImage.width || y_ClipSpace != (float)y / mImage.height);
 
         float size_x = (float)GaussianDistanceRadiusInPixel / mImage.width;
         float size_y = (float)GaussianDistanceRadiusInPixel / mImage.height;
 
-        Mat.SetTexture("_MainTex", mEnnemieSprite);
+        if (!IsArrow)
+            Mat.SetTexture("_MainTex", mEnnemieSprite);
+        else
+            RotatedBlit.SetTexture("_MainTex", mEnnemiesOOBSprite);
+
         GL.PushMatrix();
         GL.LoadOrtho();
         // activate the first shader pass (in this case we know it is the only pass)
-        Mat.SetPass(0);
+        if(!IsArrow)
+        {
+            Mat.SetPass(0);
+        }
+        else
+        {
+
+            var rotationAngle = Vector3.SignedAngle(new Vector3(x_ClipSpace, y_ClipSpace, 0) - new Vector3(0.5f, 0.5f, 0), new Vector3(0,1,0), new Vector3(0,0,1));
+            RotatedBlit.SetMatrix("_RotationMatrix", Matrix4x4.TRS(new Vector3(0.5f,0.5f,0), Quaternion.Euler(0, 0, rotationAngle - 180), Vector3.one));
+            RotatedBlit.SetPass(0);
+        }
         // draw a quad over whole screen
         GL.Begin(GL.QUADS);
         GL.TexCoord2(0f, 0f); GL.Vertex3(x_ClipSpace - size_x, y_ClipSpace - size_y, 0f);    
@@ -93,7 +110,6 @@ public class ScanBehavior : ShipElem
             PrintDot( (int)PointCenterInPixel.x, (int)PointCenterInPixel.y, DotSize, new Color32(255, 0, 0, 255));
         }
         // Print Sonar scan
-        RotatedBlit.SetMatrix("_RotationMatrix", Matrix4x4.Rotate(Quaternion.Euler(0, 0, 90)));
         RotatedBlit.SetMatrix("_RotationMatrix", Matrix4x4.TRS(new Vector3(0.5f,0.5f,0), Quaternion.Euler(0, 0, Time.time * -Speed), Vector3.one));
         RotatedBlit.SetMatrix("_MoveCenter", Matrix4x4.Translate(new Vector3(-0.5f, -0.5f, 0)));
         Graphics.Blit(mTopLayout, RTFinal, RotatedBlit);
