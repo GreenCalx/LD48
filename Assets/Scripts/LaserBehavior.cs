630a503e198bf1f18bfe53f43a3292a88aba07ec
Toffanim <marc.toffanin@gmail.com>
2021-04-27 00:07:42 +0200

    Animations Laser

--- Assets/Scripts/LaserBehavior.cs
+++ Assets/Scripts/LaserBehavior.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
 
 [RequireComponent(typeof(CapsuleCollider2D))]
 [RequireComponent(typeof(Damager))]
 public class LaserBehavior : ShipElem
 {
     CapsuleCollider2D cc2d;
-
+    public Animator Anim;
+    public Animator LaserAnim;
+    private bool lastSignal = false;
     // Start is called before the first frame update
     void Start()
     {
         cc2d = GetComponent<CapsuleCollider2D>();
         cc2d.enabled = mActivated;
+
+        lastSignal =!mActivated;
     }
 
     public void Update()
@@ -26,6 +31,36 @@ public class LaserBehavior : ShipElem
     {
         DisplayElem(mActivated);
         cc2d.enabled = mActivated;
+    }
+
+    public override void DisplayElem(bool signal)
+    {
+        if (signal != lastSignal)
+        {
+            lastSignal = signal;
+            base.DisplayElem(signal);
+
+            if (signal)
+            {
+                Anim.SetTrigger("SHOT");
+                Anim.SetBool("isShooting", true);
+                Anim.SetBool("isMirror", false);
+                LaserAnim.SetTrigger("SHOT");
+                LaserAnim.SetBool("isShooting", true);
+                LaserAnim.SetBool("isMirror", false);
+
+            }
+            else
+            {
+                Anim.SetTrigger("SHOT");
+                Anim.SetBool("isShooting", false);
+                Anim.SetBool("isMirror", true);
+                LaserAnim.SetTrigger("SHOT");
+                LaserAnim.SetBool("isShooting", false);
+                LaserAnim.SetBool("isMirror", true);
+
+            }
+        }
     }
 
     void OnTriggerStay2D(Collider2D iCol)
