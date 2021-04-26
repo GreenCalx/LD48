b6c4bf11df367dbfd0164901be4ddfab18b6f40a
GreenCalx <romain.schoenberger@gmail.com>
2021-04-26 11:13:32 +0200

    tweaks on spawns, enemy missiles no longer moving ship, better turrets wip

---
+++ Assets/Scripts/ThreatMissile.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreatMissile : Missile
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D iCol)
+    {
+        if (iCol.GetComponent<ShipBehavior>())
+        {
+            Damageable d = iCol.GetComponent<Damageable>();
+            Damager dmger = GetComponent<Damager>();
+            if (!!d)
+                d.HitMe(dmger.mDamage);
+            explode();
+        } 
+    }
+
+    void OnTriggerStay2D(Collider2D iCol)
+    {
+        if (iCol.GetComponent<ShipBehavior>())
+        {
+            Damageable d = iCol.GetComponent<Damageable>();
+            Damager dmger = GetComponent<Damager>();
+            if (!!d)
+                d.HitMe(dmger.mDamage);
+            explode();
+        } 
+    }
+
+}
